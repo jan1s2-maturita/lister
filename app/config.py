53ce3a637359badf7816242e9ceefd43625e9fff
Jan Str <janstr06@gmail.com>
2024-11-17 22:16:48 +0100

    update config

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 PUBLIC_KEY_PATH = os.environ.get('PUBLIC_KEY_PATH', "public.pem")
-REDIS_HOST = 'redis' if DEBUG else os.environ.get('REDIS_HOST')
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_USER = 'redis_user' if DEBUG else os.environ.get('REDIS_USER')
-REDIS_PASSWORD = 'redis_password' if DEBUG else os.environ.get('REDIS_PASSWORD')
+REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
+REDIS_DB = int(os.environ.get('REDIS_DB', '0'))
+REDIS_USER = os.environ.get('REDIS_USER', 'redis_user')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', 'redis_password')
 
